98167b3c3fde3c2d121375a05b8773d0b77be3e3
WLDCH <wchrimni@gmail.com>
2022-08-10 20:31:50 +0200

    Add preprocess_data function

--- train_model.py
+++ train_model.py
@@ -1,21 +1,17 @@
-from prefect import task, flow
-from prefect.task_runners import SequentialTaskRunner
-
-import mlflow
-
-import pandas as pd
 import datetime
 
+import mlflow
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-
-
+from prefect import flow, task
+from prefect.task_runners import SequentialTaskRunner
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
 # mlflow.set_tracking_uri("sqlite:///mlflow.db")
 # mlflow.set_experiment("covid-deaths-prediction")
+
 
 @task
 def scrap_data(scrap_covid_indicator=True, scrap_covid_test=True):
@@ -23,44 +19,94 @@ def scrap_data(scrap_covid_indicator=True, scrap_covid_test=True):
 
     if scrap_covid_indicator:
         # Scraps covid_indicator dataset
-        covid_indicator_page = requests.get('https://www.data.gouv.fr/fr/datasets/synthese-des-indicateurs-de-suivi-de-lepidemie-covid-19/')
-        soup_indicator = BeautifulSoup(covid_indicator_page.content, 'html.parser')
-        covid_indicator_path = soup_indicator.find_all('dd', {"class": "fr-ml-0 fr-col-8 fr-col-md-9 fr-col-lg-10 text-overflow-ellipsis"})[-1].find('a', href=True)['href']
+        covid_indicator_page = requests.get(
+            "https://www.data.gouv.fr/fr/datasets/synthese-des-indicateurs-de-suivi-de-lepidemie-covid-19/"
+        )
+        soup_indicator = BeautifulSoup(covid_indicator_page.content, "html.parser")
+        covid_indicator_path = soup_indicator.find_all(
+            "dd",
+            {
+                "class": "fr-ml-0 fr-col-8 fr-col-md-9 fr-col-lg-10 text-overflow-ellipsis"
+            },
+        )[-1].find("a", href=True)["href"]
         covid_indicator = pd.read_csv(covid_indicator_path)
 
-    if scrap_covid_test
+    if scrap_covid_test:
         # Scraps covid_test dataset
         options = Options()
         options.headless = True
         driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
         driver.implicitly_wait(0.5)
-        driver.get("https://www.data.gouv.fr/fr/datasets/donnees-de-laboratoires-pour-le-depistage-a-compter-du-18-05-2022-si-dep/")
-        bt = driver.find_element(by='xpath', value="//*[@id='app']/main/section[4]/div/div/section[1]/div/nav/ul/li[4]/a")
+        driver.get(
+            "https://www.data.gouv.fr/fr/datasets/donnees-de-laboratoires-pour-le-depistage-a-compter-du-18-05-2022-si-dep/"
+        )
+        bt = driver.find_element(
+            by="xpath",
+            value="//*[@id='app']/main/section[4]/div/div/section[1]/div/nav/ul/li[4]/a",
+        )
         bt.click()
-        pa = driver.find_element(by='xpath', value="//*[@id='resource-4e8d826a-d2a1-4d69-9ed0-b18a1f3d5ce2']/dl/div[2]/dd/a")
-        covid_test_path = pa.get_attribute('href')
-        covid_test = pd.read_csv(covid_test_path, sep=';')
+        pa = driver.find_element(
+            by="xpath",
+            value="//*[@id='resource-4e8d826a-d2a1-4d69-9ed0-b18a1f3d5ce2']/dl/div[2]/dd/a",
+        )
+        covid_test_path = pa.get_attribute("href")
+        covid_test = pd.read_csv(covid_test_path, sep=";")
         driver.quit()
 
-    return(covid_indicator, covid_test)
+    return (covid_indicator, covid_test)
 
 
 @task
 def read_data(covid_indicator_path=None, covid_test_path=None):
     if covid_indicator_path is None:
-        covid_indicator_df = scrap_data(scrap_covid_indicator=True, scrap_covid_test=False)
+        covid_indicator_df = scrap_data(
+            scrap_covid_indicator=True, scrap_covid_test=False
+        )
 
     if covid_test_path is None:
         covid_test_df = scrap_data(scrap_covid_indicator=False, scrap_covid_test=True)
 
-    return(covid_indicator_df, covid_test_df)
-
-        
+    return (covid_indicator_df, covid_test_df)
 
 
+@task
+def preprocess_data(covid_indicator_df, covid_test_df):
 
+    features_indicator = [
+        "hosp",
+        "incid_hosp",
+        "rea",
+        "incid_rea",
+        "rad",
+        "incid_rad",
+        "pos",
+        "pos_7j",
+        "tx_pos",
+        "tx_incid",
+        "TO",
+        "R",
+    ]
+    features_test = ["pop", "P", "T", "Ti", "Tp", "Td"]
+    features = features_indicator + features_test
+    target = ["incid_dchosp"]
 
-scrap_data()
+    covid_test_df.loc[:, features_test] = covid_test_df.loc[:, features_test].applymap(
+        lambda row: float(row.replace(",", "."))
+    )
 
+    covid_df = pd.merge(
+        left=covid_indicator_df,
+        right=covid_test_df,
+        left_on="date",
+        right_on="jour",
+        how="inner",
+    )
 
+    covid_df = covid_df[["date"] + features + target].dropna()
+    covid_df.loc[:, "date"] = covid_df.date.apply(
+        lambda row: datetime.datetime.strptime(row, "%Y-%m-%d")
+    )
+    covid_df.set_index("date", inplace=True)
+
+    return covid_df
 
