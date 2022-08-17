b7d102e02ec90c717dcc2e62d17e85a80c824b56
WLDCH <wchrimni@gmail.com>
2022-08-17 22:13:09 +0200

    Format with black & isort

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -1,10 +1,13 @@
+import numpy as np
+import pandas as pd
+
 import train_model
 
-import pandas as pd
-import numpy as np
 
 def test_scrap_covid_indicator():
-    expected_df = pd.read_csv('data/table-indicateurs-open-data-france-2022-08-05-19h01.csv')
+    expected_df = pd.read_csv(
+        "data/table-indicateurs-open-data-france-2022-08-05-19h01.csv"
+    )
     actual_df = train_model.scrap_covid_indicator.fn()
 
     expected_df.dropna(inplace=True)
@@ -12,11 +15,11 @@ def test_scrap_covid_indicator():
 
     print(expected_df)
     print(actual_df)
-    assert expected_df.equals(actual_df.iloc[:expected_df.shape[0], :])
+    assert expected_df.equals(actual_df.iloc[: expected_df.shape[0], :])
 
 
 def test_scrap_covid_test():
-    expected_df = pd.read_csv('data/sp-fra-jour-2022-08-05-19h01.csv', sep=';')
+    expected_df = pd.read_csv("data/sp-fra-jour-2022-08-05-19h01.csv", sep=";")
     actual_df = train_model.scrap_covid_test.fn()
 
     expected_df.dropna(inplace=True)
@@ -25,26 +28,34 @@ def test_scrap_covid_test():
     # covid tests value are updated so we can't test dataframe equality
     assert np.all(expected_df.columns == actual_df.columns)
 
+
 def test_preprocess_data():
 
-    expected_df = pd.read_csv('data/covid_df.csv', index_col='date')
-    covid_indicator = pd.read_csv('data/table-indicateurs-open-data-france-2022-08-05-19h01.csv')
-    covid_test = pd.read_csv('data/sp-fra-jour-2022-08-05-19h01.csv', sep=';')
-    actual_df = train_model.preprocess_data.fn(covid_indicator_df=covid_indicator, covid_test_df=covid_test)
+    expected_df = pd.read_csv("data/covid_df.csv", index_col="date")
+    covid_indicator = pd.read_csv(
+        "data/table-indicateurs-open-data-france-2022-08-05-19h01.csv"
+    )
+    covid_test = pd.read_csv("data/sp-fra-jour-2022-08-05-19h01.csv", sep=";")
+    actual_df = train_model.preprocess_data.fn(
+        covid_indicator_df=covid_indicator, covid_test_df=covid_test
+    )
 
-    #same as previous test
+    # same as previous test
     assert np.all(expected_df.columns == actual_df.columns)
+
 
 def test_create_preprocess_time_series():
 
-    covid_df = pd.read_csv('data/covid_df.csv')
-    (y_scaled,
-    y_train,
-    y_val,
-    past_cov_train,
-    past_cov_val,
-    target_scaler,
-    past_cov_scaler) =  train_model.create_preprocess_time_series.fn(covid_df) 
+    covid_df = pd.read_csv("data/covid_df.csv")
+    (
+        y_scaled,
+        y_train,
+        y_val,
+        past_cov_train,
+        past_cov_val,
+        target_scaler,
+        past_cov_scaler,
+    ) = train_model.create_preprocess_time_series.fn(covid_df)
 
     assert y_scaled == y_train.concatenate(y_val)
     assert np.all(y_scaled <= 1) & np.all(y_scaled >= 0)
