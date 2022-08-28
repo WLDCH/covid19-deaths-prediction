c05c4bde2609c86d4ed5dfa961b96bc99c06e75e
WLDCH <wchrimni@gmail.com>
2022-08-28 17:17:04 +0200

    Add remote model registery

--- train_model.py
+++ train_model.py
@@ -1,70 +1,63 @@
-import datetime
-
 import matplotlib as mpl
 
 mpl.use("Agg")
-
-
-import pickle
-
 import mlflow
-from darts.models import (BlockRNNModel,
-                          NBEATSModel, NHiTSModel, RandomForest,
-                          RegressionModel, TCNModel,
-                          TransformerModel)
+from darts.models import BlockRNNModel, TCNModel, TransformerModel
+from google.cloud import storage
 from prefect import flow, task
-from sklearn.ensemble import RandomForestRegressor
 
-from utils.read_preprocess import read_data, preprocess_data
-from utils.train_utils import create_preprocess_time_series_train_val, compute_metrics, plot
-from utils.variables import EXPERIMENT_NAME, TRACKING_URI, length_pred, features_indicator, features_test, features, target
+from utils.read_preprocess import preprocess_data, read_data
+from utils.train_utils import (
+    compute_metrics,
+    create_preprocess_time_series_train_val,
+    plot,
+)
+from utils.variables import (
+    EXPERIMENT_NAME,
+    TRACKING_URI,
+    features,
+    features_indicator,
+    features_test,
+    length_pred,
+    target,
+)
 
+storage_client = storage.Client()
+bucket = storage_client.get_bucket("covid19-deaths-prediction-bucket")
 mlflow.set_tracking_uri(TRACKING_URI)
 mlflow.set_experiment(EXPERIMENT_NAME)
+
 
 @task
 def train_model(y, y_train, y_val, past_cov_train, past_cov_val, model_name):
 
-    if model_name == "Regression":
-        model = RegressionModel(
-            lags=[-1, -2, -3, -4, -5, -6, -7],
-            lags_past_covariates=[-1],  # , -2, -3, -4, -5, -6, -7],
-            model=RandomForestRegressor(),
-        )
-    elif model_name == "RandomForest":
-        model = RandomForest(
-            lags=[-1, -2, -3, -4, -5, -6, -7],
-            lags_past_covariates=[-1, -2, -3, -4, -5, -6, -7],
-        )
-    elif model_name == "N-BEATS":
-        model = NBEATSModel(
-            input_chunk_length=length_pred,
-            output_chunk_length=length_pred,
-        )
-    elif model_name == "N-HiTS":
-        model = NHiTSModel(
-            input_chunk_length=length_pred,
-            output_chunk_length=length_pred,
-        )
-    elif model_name == "TCN":
+    if model_name == "TCN":
         model = TCNModel(
             input_chunk_length=length_pred + 1,
             output_chunk_length=length_pred,
+            save_checkpoints=True,
+            force_reset=True,
+            work_dir="checkpoints/",
+            model_name=model_name,
         )
     elif model_name == "Transformer":
         model = TransformerModel(
             input_chunk_length=length_pred,
             output_chunk_length=length_pred,
+            save_checkpoints=True,
+            force_reset=True,
+            work_dir="checkpoints/",
+            model_name=model_name,
         )
     elif model_name == "RNN":
         model = BlockRNNModel(
-            input_chunk_length=length_pred, output_chunk_length=length_pred, model="RNN"
-        )
-    elif model_name == "LSTM":
-        model = BlockRNNModel(
             input_chunk_length=length_pred,
             output_chunk_length=length_pred,
-            model="LSTM",
+            model="RNN",
+            save_checkpoints=True,
+            force_reset=True,
+            work_dir="checkpoints/",
+            model_name=model_name,
         )
 
     model.fit(series=y_train, past_covariates=past_cov_train)
@@ -75,8 +68,6 @@ def train_model(y, y_train, y_val, past_cov_train, past_cov_val, model_name):
     return (model, y_pred)
 
 
-
-
 @flow
 def main(
     model_name,
@@ -85,12 +76,9 @@ def main(
     covid_indicator_path=None,
     covid_test_path=None,
 ):
-    with mlflow.start_run(run_name=f"{model_name}_run"):
-        # global features_indicator
-        # global features_test
-        # global features
-        # global target
-
+    with mlflow.start_run(run_name=f"{model_name}_run") as run:
+        run_id = run.info.run_id
+        print(run_id)
         covid_indicator_df, covid_test_df = read_data(
             covid_indicator_path, covid_test_path
         )
@@ -123,18 +111,14 @@ def main(
             output_chunk_length=output_chunk_length,
             model_name=model_name,
         )
-        plot(y, y_pred, model_name)
-        model.save(f"models/{model_name}Model")
-        with open(f"models/scalers", "wb") as f_out:
-            pickle.dump((target_scaler, past_cov_scaler), f_out)
+        # plot(y, y_pred, model_name)
+        mlflow.log_artifacts("checkpoints/")
+        model.save(f"models/MLmodel")
+        mlflow.log_artifact(f"models/MLmodel", "model")
+        mlflow.register_model(f"runs:/{run_id}/model", f"{model_name}Model")
 
 
 if __name__ == "__main__":
-    # main(model_name="Regression")
-    # main(model_name='RandomForest')
-    # main(model_name="N-BEATS")
-    # main(model_name="N-HiTS")
-    # main(model_name="TCN")
+    main(model_name="TCN")
     main(model_name="Transformer")
-    # main(model_name='RNN')
-    # main(model_name='LSTM')
+    main(model_name="RNN")
